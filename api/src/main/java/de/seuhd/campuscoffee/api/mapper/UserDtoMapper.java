d7a8194d6e7528b256fa99a8256d37075311771a
RythenGlyth <rythenglyth@gmail.com>
2025-12-02 12:57:41 +0100

    add missing bean annotation to UserDtoMapper to fix compilation errors

--- api/src/main/java/de/seuhd/campuscoffee/api/mapper/UserDtoMapper.java
+++ api/src/main/java/de/seuhd/campuscoffee/api/mapper/UserDtoMapper.java
@@ -1,8 +1,13 @@
 package de.seuhd.campuscoffee.api.mapper;
 
+import org.mapstruct.Mapper;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+
 import de.seuhd.campuscoffee.api.dtos.UserDto;
 import de.seuhd.campuscoffee.domain.model.User;
 
+@Mapper(componentModel = "spring")
+@ConditionalOnMissingBean
 public interface UserDtoMapper {
     UserDto fromDomain(User source);
     User toDomain(UserDto source);
