2237f636aade2c45135bb15d91c57d0d51b1e8e7
RythenGlyth <rythenglyth@gmail.com>
2025-12-02 12:29:44 +0100

    Implement UserDtoMapper interface and define User domain object fields

--- domain/src/main/java/de/seuhd/campuscoffee/domain/model/User.java
+++ domain/src/main/java/de/seuhd/campuscoffee/domain/model/User.java
@@ -4,10 +4,21 @@ import lombok.Builder;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import org.jspecify.annotations.NonNull;
+import org.jspecify.annotations.Nullable;
 
 @Builder(toBuilder = true)
 public record User (
-        //TODO: Implement user domain object
+        @Nullable Long id, // id is null when creating a new user
+        @Nullable LocalDateTime createdAt, // is null when using DTO to create a new user
+        @Nullable LocalDateTime updatedAt, // is set when creating or updating a user
+        @NonNull String loginName,
+        @NonNull String emailAddress,
+        @NonNull String firstName,
+        @NonNull String lastName
+
 ) implements Serializable { // serializable to allow cloning (see TestFixtures class).
     @Serial
     private static final long serialVersionUID = 1L;
