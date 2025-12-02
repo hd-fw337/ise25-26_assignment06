cd8ffc95ccdd8a8015b7e3611d47666b5168030c
RythenGlyth <rythenglyth@gmail.com>
2025-12-02 12:52:35 +0100

    uncomment everything

--- application/src/test/java/de/seuhd/campuscoffee/tests/system/UsersSystemTests.java
+++ application/src/test/java/de/seuhd/campuscoffee/tests/system/UsersSystemTests.java
@@ -1,16 +1,23 @@
 package de.seuhd.campuscoffee.tests.system;
 
+import static de.seuhd.campuscoffee.tests.SystemTestUtils.Requests.userRequests;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import de.seuhd.campuscoffee.domain.model.User;
+import de.seuhd.campuscoffee.domain.tests.TestFixtures;
+
 public class UsersSystemTests extends AbstractSysTest {
 
-    //TODO: Uncomment once user endpoint is implemented
+   @Test
+   void createUser() {
+       User userToCreate = TestFixtures.getUserListForInsertion().getFirst();
+       User createdUser = userDtoMapper.toDomain(userRequests.create(List.of(userDtoMapper.fromDomain(userToCreate))).getFirst());
 
-//    @Test
-//    void createUser() {
-//        User userToCreate = TestFixtures.getUserListForInsertion().getFirst();
-//        User createdUser = userDtoMapper.toDomain(userRequests.create(List.of(userDtoMapper.fromDomain(userToCreate))).getFirst());
-//
-//        assertEqualsIgnoringIdAndTimestamps(createdUser, userToCreate);
-//    }
+       assertEqualsIgnoringIdAndTimestamps(createdUser, userToCreate);
+   }
 
     //TODO: Add at least two additional tests for user operations
 
